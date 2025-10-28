5c4c1cdc737972e3643d7485f15ebb602092ad42
akcoldheart <davenye2@gmail.com>
2025-10-28 15:40:56 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-<button>Click me</button>;
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
