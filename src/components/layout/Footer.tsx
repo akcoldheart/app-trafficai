54036d9953e046588df57ca85aa000112ef9ef67
KEDAR KUMAR <kedarkumar@KEDARs-MacBook-Pro.local>
2026-01-29 17:00:26 +0530

    New Pixel design/audience logic 1.0.13

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,19 +4,10 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="footer footer-transparent d-print-none">
+    <footer className="footer footer-transparent d-print-none" style={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 100 }}>
       <div className="container-xl">
-        <div className="row text-center align-items-center flex-row-reverse">
-          <div className="col-lg-auto ms-lg-auto">
-            <ul className="list-inline list-inline-dots mb-0">
-              <li className="list-inline-item">
-                <Link href="/settings" className="link-secondary">
-                  Settings
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-12 col-lg-auto mt-3 mt-lg-0">
+        <div className="row text-center align-items-center">
+          <div className="col-12">
             <ul className="list-inline list-inline-dots mb-0">
               <li className="list-inline-item">
                 Copyright &copy; {currentYear}{' '}
